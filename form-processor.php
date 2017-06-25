dcfe9ab003ddf9f2402f5aa28e041d80405b7708
Brian Simoni <simonibc@vcu.edu>
2017-06-24 23:39:52 -0400

    Basic email collection should work now

--- form-processor.php
+++ form-processor.php
@@ -11,12 +11,17 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+// Prepared statement
 $stmt = $conn->prepare("INSERT INTO emails (email) VALUES (?)");
 $stmt->bind_param("s", $email);
 
 // set parameters and execute
 
 $email = $_POST["email"];
+if(!filter_var(email, FILTER_VALIDATE_EMAIL)) {
+    die("Invalid email submitted: ({$email})");
+}
+
 $stmt->execute();
 
 $conn->close();
