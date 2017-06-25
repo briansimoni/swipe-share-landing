349ffa8cae9ea83155b3b9cf1dadc711a7ef3070
Brian Simoni <simonibc@vcu.edu>
2017-06-24 23:47:02 -0400

    Turn on error reporting

--- form-processor.php
+++ form-processor.php
@@ -1,4 +1,10 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 $servername = "localhost";
 $username = "swipe-share";
 $password = "-w-Ln`%7V52kSRzE";
