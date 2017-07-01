97057337d0aa3539c296c92ab3d3af22e99a7128
Brian Simoni <simonibc@vcu.edu>
2017-07-01 18:45:57 -0400

    Setup admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -4,4 +4,24 @@ if($_SESSION["loggedIn"] !== true) {
     die("nope");
 }
 
-echo "you're an admin";
+$servername = "localhost";
+$username = "swipe-share";
+$password = "-w-Ln`%7V52kSRzE";
+
+$conn = new mysqli($servername, $username, $password);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT email FROM swipeshare.emails";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "Email: " . $row["email"] . "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
