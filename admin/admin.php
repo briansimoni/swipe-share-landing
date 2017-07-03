26831af0e3eb11728b7f4617ddce8e7f572b0436
Brian Simoni <simonibc@vcu.edu>
2017-07-03 14:52:40 -0400

    Modified admin page

--- admin/admin.php
+++ admin/admin.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
 if($_SESSION["loggedIn"] !== true) {
-    die("nope");
+    header("Location: /admin/login.php");
+    die();
 }
 
 $servername = "localhost";
@@ -17,11 +18,39 @@ if ($conn->connect_error) {
 $sql = "SELECT email FROM swipeshare.emails";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-        echo "Email: " . $row["email"] . "<br>";
-    }
-} else {
-    echo "0 results";
-}
-$conn->close();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- TODO: implement favicon!!! -->
+    <link rel="icon" href="../../favicon.ico">
+    <link rel="stylesheet" href="/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/styles/style.css">
+
+    <title>Swipe Share App</title>
+</head>
+
+<body>
+
+<?php if ($result->num_rows > 0) { ?>
+
+    <ol>
+    <?php while($row = $result->fetch_assoc()) { ?>
+
+        <td><?php echo $row["email"] ?></td>
+
+
+    <?php } ?>
+<?php } ?>
+<?php $conn->close(); ?>
+
+    </ol>
+
+</body>
